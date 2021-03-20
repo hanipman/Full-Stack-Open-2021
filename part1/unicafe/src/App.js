bfff19b9845dc3c01c3bb3271c73679f94a2e709
Chris Boado <boadoct@vt.edu>
2021-03-19 22:18:39 -0400

    1.7 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,20 +1,45 @@
 import React, { useState } from 'react'
 
 function App() {
-	const [good, setGood] = useState(0)
-  	const [neutral, setNeutral] = useState(0)
-  	const [bad, setBad] = useState(0)
+	const [stats, setStats] = useState(
+		{
+			good: 0,
+			neutral: 0,
+			bad: 0,
+			all: 0,
+			average: 0,
+			positive: 0
+		}
+	)
 
 	const handleGood = () => {
-		setGood(good + 1)
+		setStats({
+			...stats,
+			good: stats.good + 1,
+			all: stats.all + 1,
+			average: (stats.good - stats.bad + 1)/(stats.all + 1),
+			positive: 100 * (stats.good + 1)/(stats.all + 1)
+		})
 	}
 
 	const handleNeutral = () => {
-		setNeutral(neutral + 1)
+		setStats({
+			...stats,
+			neutral: stats.neutral + 1,
+			all: stats.all + 1,
+			average: (stats.good - stats.bad + 1)/(stats.all + 1),
+			positive: 100 * (stats.good)/(stats.all + 1)
+		})
 	}
 
 	const handleBad = () => {
-		setBad(bad + 1)
+		setStats({
+			...stats,
+			bad: stats.bad + 1,
+			all: stats.all + 1,
+			average: (stats.good - stats.bad - 1)/(stats.all + 1),
+			positive: 100 * (stats.good)/(stats.all + 1)
+		})
 	}
 
 	return (
@@ -24,9 +49,12 @@ function App() {
 			<button onClick={handleNeutral}>neutral</button>
 			<button onClick={handleBad}>bad</button>
 			<h1>statistics</h1>
-			<p>good {good}</p>
-			<p>neutral {neutral}</p>
-			<p>bad {bad}</p>
+			<p>good {stats.good}</p>
+			<p>neutral {stats.neutral}</p>
+			<p>bad {stats.bad}</p>
+			<p>all {stats.all}</p>
+			<p>average {stats.average}</p>
+			<p>positive {stats.positive} %</p>
 		</div>
 	)
 }
