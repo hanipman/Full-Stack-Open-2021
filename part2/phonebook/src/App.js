e0bb0f637e7eef4fe5384dc7f953d22d4a992f4d
Chris Boado <boadoct@vt.edu>
2021-03-22 15:19:10 -0400

    2.11 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Filter = ({ filter, handleFilter }) => {
 	return (
@@ -34,18 +35,20 @@ const Persons = ({ persons, filter }) => {
 }
 
 const App = () => {
-	const [ persons, setPersons ] = useState([
-		{ name: 'Arto Hellas', number: '040-123456' },
-    	{ name: 'Ada Lovelace', number: '39-44-5323523' },
-    	{ name: 'Dan Abramov', number: '12-43-234345' },
-    	{ name: 'Mary Poppendieck', number: '39-23-6423122' }
-	]) 
+	const [ persons, setPersons ] = useState([]) 
 	const [ newEntry, setEntry ] = useState({
 		name: '',
 		number: ''
 	})
 	const [ filter, setFilter ] = useState('')
 	
+	useEffect(() => {
+		axios.get('http://localhost:3001/persons')
+			.then(response => {
+				setPersons(response.data)
+			})
+	}, [])
+
 	const handleAdd = (event) => {
 		event.preventDefault()
 		if (persons.filter(person => person.name === newEntry.name).length > 0) {
