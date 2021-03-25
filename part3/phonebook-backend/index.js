dacd056a48e6057ef1aba58c52d1274e3c3b932c
Chris Boado <boadoct@vt.edu>
2021-03-25 14:31:12 -0400

    3.9 done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,9 @@
+const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan((tokens, request, response) => {
 	return [
