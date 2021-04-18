4c5a654857c916d408db613c1e23c10332b99458
Chris Boado <boadoct@vt.edu>
2021-04-18 19:01:52 -0400

    2.20 done

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-	const authorization = request.get('authorization')
-	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-		return authorization.substring(7)
-	}
-	return null
-}
-
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog
 		.find({})
@@ -24,7 +16,7 @@ blogsRouter.post('/', async (request, response) => {
 		response.status(400).json({ error: 'malformed title or author' })
 		return
 	}
-	const token = getTokenFrom(request)
+	const token = request.token
 	const decodedToken = jwt.verify(token, process.env.SECRET)
 	if (!token || !decodedToken.id) {
 		return response.status(401).json({
