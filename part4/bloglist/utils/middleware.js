65ada438c61438138885ce41e40066a1c308db43
Chris Boado <boadoct@vt.edu>
2021-04-19 19:08:54 -0400

    4.22 done

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -37,7 +37,6 @@ const errorHandler = (error, request, response, next) => {
 	logger.error(error.message)
 	
 	if (error.name === 'CastError') {
-		
 		return response.status(400).send({ error: 'malformed id' })
 	}
 	else if (error.name === 'ValidationError') {
