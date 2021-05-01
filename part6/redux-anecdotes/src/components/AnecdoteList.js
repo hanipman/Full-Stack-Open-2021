d0812f02ed185be233a17b0c1f6a21e79faf23f7
Chris Boado <boadoct@vt.edu>
2021-05-01 04:11:14 -0400

    6.12 done

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -4,7 +4,9 @@ import { incrementVote } from '../reducers/anecdoteReducer'
 import { voteNotification, hideNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
-	const anecdotes = useSelector(state => state.anecdotes)
+	const anecdotes = useSelector(state => {
+		return state.anecdotes.filter(anecdote => anecdote.content.includes(state.filter))
+	})
 	const dispatch = useDispatch()
 
 	const vote = (id, content) => {
@@ -18,17 +20,20 @@ const AnecdoteList = () => {
 
 	return (
 		<div>
-			{anecdotes.map(anecdote =>
-				<div key={anecdote.id}>
-					<div>
-						{anecdote.content}
+			{anecdotes.map(anecdote => {
+				return (
+					<div key={anecdote.id}>
+						<div>
+							{anecdote.content}
+						</div>
+						<div>
+							has {anecdote.votes}
+							<button onClick={() => vote(anecdote.id, anecdote.content)}>vote</button>
+						</div>
 					</div>
-					<div>
-						has {anecdote.votes}
-						<button onClick={() => vote(anecdote.id, anecdote.content)}>vote</button>
-					</div>
-				</div>
-			)}
+				)
+			
+			})}
 		</div>
 	)
 }
